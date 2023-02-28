53403ae17153acb3a263de8869991372f6a46395
Nikolay Naumov <naumovnick@mail.ru>
2023-02-28 21:09:09 +0300

    initial

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int firstYear = 1922;
+        int lastYear = 2022;
+
+
+        for (int year = firstYear; year <= lastYear; year = year + 1) {
+            if (year % 4 != 0) {
+                continue;
+            }
+            System.out.println(year);
+            for (int month = 1; month <= 12; month = month + 1) {
+                String zero = month < 10 ? "0" : "";
+                System.out.println("29. " + month + "." + year);
+            }
+        }
+    }
+}
